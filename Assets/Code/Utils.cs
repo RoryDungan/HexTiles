b4ca9b8dda971d319aacac120b945e5149ea0b72
Rory Dungan <rory.dungan@realseriousgames.com>
2016-06-07 12:45:06 +1000

    Added game object extensions class
    Contains functionality for finding components in children with proper error handling.

---
+++ Assets/Code/Utils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class Utils
+{
+    /// <summary>
+    /// Get a specified type of component from the specified object.
+    /// Throws an exception if the component doesn't exist or if there are multiple and it is ambiguous which to choose.
+    /// </summary>
+    public static ComponentT ExpectComponentInChildren<ComponentT>(this GameObject parent) where ComponentT : Component
+    {
+        var components = parent.GetComponentsInChildren<ComponentT>();
+        if (components.Length < 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a {1} in children but none was found.", parent.name, typeof(ComponentT).Name));
+        }
+        else if (components.Length > 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a single {1} in children but multiple were found.", parent.name, typeof(ComponentT).Name));
+        }
+        return components[0];
+    }
+
+    /// <summary>
+    /// Get a named componet on a child of the specified object.
+    /// Throws an exception if the component doesn't exist or if there are multiple and it is ambiguous which to choose.
+    /// </summary>
+    public static ComponentT ExpectComponentInChildren<ComponentT>(this GameObject parent, string name) where ComponentT : Component
+    {
+        var components = parent.GetComponentsInChildren<ComponentT>()
+            .Where(component => component.gameObject.name == name)
+            .ToArray();
+
+        if (components.Length < 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a {1} in children but none was found.", parent.name, typeof(ComponentT).Name));
+        }
+        else if (components.Length > 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a single {1} in children but multiple were found.", parent.name, typeof(ComponentT).Name));
+        }
+        return components[0];
+    }
+
+    /// <summary>
+    /// Get a named object from the children of the specified object.
+    /// Throws an exception if the GameObject doesn't exist or if there are multiple and it is ambiguous which to choose.
+    /// </summary>
+    public static GameObject ExpectObjectInChildren(this GameObject parent, string name)
+    {
+        var components = parent.GetComponentsInChildren<Transform>()
+            .Where(trans => trans.gameObject.name == name)
+            .ToArray();
+
+        if (components.Length < 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a child named {1} but none was found.", parent.name, name));
+        }
+        else if (components.Length > 1)
+        {
+            throw new ApplicationException(string.Format("Object {0} expected a single child named {1} but multiple were found.", parent.name, name));
+        }
+        return components[0].gameObject;
+    }
+}
