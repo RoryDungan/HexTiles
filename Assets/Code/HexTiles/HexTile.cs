3b2329312ac83e6cd2eb9fb0a295b8b48d91f898
Rory Dungan <rorydungan@gmail.com>
2016-06-17 20:50:27 +1000

    Started working on the hex tile system
    Got code for generating a single hex tile working.

---
+++ Assets/Code/HexTiles/HexTile.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexTile : MonoBehaviour
+{
+    /// <summary>
+    /// Total diameter of the hex.
+    /// </summary>
+    public float size = 1f;
+
+    private Vector3[] vertices;
+
+    /// <summary>
+    /// sqrt(3)/2
+    /// The ratio of a flat-topped hexagon's height to its width.
+    /// </summary>
+    private static readonly float hexHeightToWidth = 0.86602540378f;
+
+    /// <summary>
+    /// Create the mesh used to render the hex.
+    /// </summary>
+    [ContextMenu("Generate mesh")]
+    public void GenerateMesh()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+        mesh.name = "Procedural hex tile";
+
+        vertices = new Vector3[6];
+        var uv = new Vector2[vertices.Length];
+        var tangents = new Vector4[vertices.Length];
+
+        // Calculate vertices.
+        vertices[0].Set(-0.5f  * size,  0f,  0f);
+        vertices[1].Set(-0.25f * size,  0f,  0.5f * hexHeightToWidth * size);
+        vertices[2].Set( 0.25f * size,  0f,  0.5f * hexHeightToWidth * size);
+        vertices[3].Set( 0.5f  * size,  0f,  0f);
+        vertices[4].Set( 0.25f * size,  0f, -0.5f * hexHeightToWidth * size);
+        vertices[5].Set(-0.25f * size,  0f, -0.5f * hexHeightToWidth * size);
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            tangents[i].Set(1f, 0f, 0f, -1f);
+        }
+
+        mesh.vertices = vertices;
+
+        // Calculate triangles.
+        mesh.triangles = new int[] {
+            0, 1, 5,
+            1, 4, 5,
+            1, 2, 4,
+            2, 3, 4
+        };
+
+        mesh.RecalculateNormals();
+    }
+
+    void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+
+
+        Gizmos.color = Color.black;
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
---
+++ Assets/Code/HexTiles/HexTileMap.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexTileMap : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnDrawGizmos()
+    {
+        DrawGizmos(false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        DrawGizmos(true);
+    }
+
+    void DrawGizmos(bool selected)
+    {
+        if (selected)
+        {
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            Gizmos.color = Color.gray;
+        }
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up * 4);
+    }
+}
