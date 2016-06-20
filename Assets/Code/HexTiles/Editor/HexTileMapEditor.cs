a91c3d4bf97273e490a1734432083db0ff1c403b
Rory Dungan <rorydungan@gmail.com>
2016-06-20 22:47:16 +1000

    Started working on editor for hex tile maps

---
+++ Assets/Code/HexTiles/Editor/HexTileMapEditor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+namespace HexTiles.Editor
+{
+    /// <summary>
+    /// Editor for hex tile maps. Contains code for interacting with and painting tiles
+    /// in the editor.
+    /// </summary>
+    [CustomEditor(typeof(HexTileMap))]
+    public class HexTileMapEditor : UnityEditor.Editor
+    {
+        private Texture2D selectButtonIcon;
+        private Texture2D paintButtonIcon;
+        private Texture2D eraseButtonIcon;
+        private Texture2D settingsButtonIcon;
+
+        void OnSceneGUI()
+        {
+            //Debug.Log(Event.current.mousePosition);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            var toolIcons = new GUIContent[]
+            {
+                new GUIContent(selectButtonIcon, "Select"),
+                new GUIContent(paintButtonIcon, "Paint"),
+                new GUIContent(eraseButtonIcon, "Delete"),
+                new GUIContent(settingsButtonIcon, "Settings")
+            };
+
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Toolbar(0, toolIcons, "command");
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginVertical(EditorStyles.helpBox);
+            GUILayout.Label("Select");
+            GUILayout.Label("Pick a hex tile to manually edit its properties.", EditorStyles.wordWrappedMiniLabel);
+            GUILayout.EndVertical();
+        }
+
+        void OnEnable()
+        {
+            selectButtonIcon = LoadImage("mouse-pointer_44");
+            paintButtonIcon = LoadImage("paint-brush_44");
+            eraseButtonIcon = LoadImage("eraser_44");
+            settingsButtonIcon = LoadImage("cog_44");
+        }
+
+        Texture2D LoadImage(string resource)
+        {
+            var image = Resources.Load<Texture2D>(resource);
+            if (image == null)
+            {
+                throw new ApplicationException("Failed to load image from resource \"" + resource + "\"");
+            }
+
+            return image;
+        }
+    }
+}
