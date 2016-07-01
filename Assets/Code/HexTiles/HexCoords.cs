074cda84816eaa16e17a0804d16cafaeec27c759
Rory Dungan <rorydungan@gmail.com>
2016-07-01 16:04:53 +1000

    Made HexCoords override Equals and GetHashCode
    Needed so that we can store HexCoords in a hashtable for efficient access.

--- Assets/Code/HexTiles/HexCoords.cs
+++ Assets/Code/HexTiles/HexCoords.cs
@@ -16,5 +16,37 @@ namespace HexTiles
         public int R = 0;
 
         public float Elevation = 0f;
+
+        // override object.GetHashCode
+        // Taken from http://stackoverflow.com/questions/5221396/what-is-an-appropriate-gethashcode-algorithm-for-a-2d-point-struct-avoiding
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                var hash = 17;
+                hash = hash * 23 + Q.GetHashCode();
+                hash = hash * 23 + R.GetHashCode();
+                return hash;
+            }
+        }
+
+        // override object.Equals
+        public override bool Equals (object obj)
+        {
+            //
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+            
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var hexCoords = (HexCoords)obj;
+            return hexCoords.Q == Q && hexCoords.R == R;
+        }
     }
 }
