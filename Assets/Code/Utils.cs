9c1b5e758f27f51c27917633d7916bf9873909ea
Rory Dungan <rorydungan@gmail.com>
2016-08-01 01:47:04 +1000

    Added helper method for modulo

--- Assets/Code/Utils.cs
+++ Assets/Code/Utils.cs
@@ -120,4 +120,14 @@ public static class Utils
         }
         return components[0];
     }
+
+    /// <summary>
+    /// Returns the modulo of two floats. Needed because C#'s '%' operator
+    /// actually returns the remainder of integer division as opposed to a modulo.
+    /// See http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain 
+    /// </summary>
+    public static float Mod(float a, float b)
+    {
+        return (a % b + b) % b;
+    }
 }
