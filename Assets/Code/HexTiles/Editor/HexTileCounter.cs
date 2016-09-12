b63f9d08e5ab8d66ad0653ccc887d1853908b0b5
Rory Dungan <rorydungan@gmail.com>
2016-09-13 01:18:03 +1000

    Added simple utility for counting the number of hex tiles in a level

---
+++ Assets/Code/HexTiles/Editor/HexTileCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace HexTiles.Editor
+{
+    public class HexTileCounter : ScriptableObject
+    {
+        [MenuItem("Tools/Count hex tiles")]
+        static void CountTilesClicked()
+        {
+            var regularTileCount = GameObject.FindObjectsOfType<HexTile>().Length;
+
+            var chunks = GameObject.FindObjectsOfType<HexChunk>();
+            var chunkCount = chunks.Length;
+
+            var message = string.Format("Individual tiles: {0}\nChunks: {1}", regularTileCount, chunkCount);
+
+            EditorUtility.DisplayDialog("Hex tile count", message, "Ok");
+        }
+    }
+}
