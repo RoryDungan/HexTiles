5f483d37e87860f5bffe62e9f87972f41c209f76
Rory Dungan <rorydungan@gmail.com>
2016-09-13 00:34:32 +1000

    Completely disabled wireframe hiding feature

--- Assets/Code/HexTiles/Editor/HexTileMapEditor.cs
+++ Assets/Code/HexTiles/Editor/HexTileMapEditor.cs
@@ -213,12 +213,11 @@ namespace HexTiles.Editor
                                         new HexPosition(hex, state.PaintHeight + state.PaintOffset),
                                         hexMap.CurrentMaterial);
 
+                                    // TODO: find a way of making this work for chunks
                                     //EditorUtility.SetSelectedWireframeHidden(newTile.GetComponent<Renderer>(), !hexMap.DrawWireframeWhenSelected);
                                 }
 
                                 hexMap.SelectedTile = centerSelectedTileCoords;
-
-                                hexMap.UpdateTileChunks();
 
                                 MarkSceneDirty();
                             }
@@ -349,15 +348,18 @@ namespace HexTiles.Editor
                     {
                         ShowHelpBox("Settings", "Configure options for the whole tile map.");
 
-                        var shouldShowWireframe = EditorGUILayout.Toggle("Show wireframe outline", hexMap.DrawWireframeWhenSelected);
-                        if (shouldShowWireframe != hexMap.DrawWireframeWhenSelected)
-                        {
-                            SetWireframeVisible(shouldShowWireframe);
-                            hexMap.DrawWireframeWhenSelected = shouldShowWireframe;
+                        // TODO: This has been commented out because it doesn't currently work with the new 
+                        // hex chunks system. I'm not sure if any artists/designers even used it anyway, so 
+                        // it may not be needed anyway - Rory 13/09/2016
+                        //var shouldShowWireframe = EditorGUILayout.Toggle("Show wireframe outline", hexMap.DrawWireframeWhenSelected);
+                        //if (shouldShowWireframe != hexMap.DrawWireframeWhenSelected)
+                        //{
+                        //    SetWireframeVisible(shouldShowWireframe);
+                        //    hexMap.DrawWireframeWhenSelected = shouldShowWireframe;
 
-                            SceneView.RepaintAll();
-                            MarkSceneDirty();
-                        }
+                        //    SceneView.RepaintAll();
+                        //    MarkSceneDirty();
+                        //}
 
                         var shouldDrawPositionHandles = EditorGUILayout.Toggle("Show tile positions", hexMap.DrawHexPositionHandles);
                         if (shouldDrawPositionHandles != hexMap.DrawHexPositionHandles)
@@ -605,6 +607,8 @@ namespace HexTiles.Editor
             GUILayout.EndHorizontal();
 
             rootState.Update(Time.deltaTime);
+
+            hexMap.UpdateTileChunks();
         }
 
         /// <summary>
