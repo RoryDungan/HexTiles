dbc40e1ed7ff8bfedfb1ea5a4c1ba13720b34bd1
Rory Dungan <rorydungan@gmail.com>
2017-01-17 02:01:08 +0100

    Moved ModifiedTileInfo out into its own file

--- Assets/Code/HexTiles/HexTileMap.cs
+++ Assets/Code/HexTiles/HexTileMap.cs
@@ -567,35 +567,4 @@ namespace HexTiles
             chunks = null;
         }
     }
-
-    /// <summary>
-    /// Information about a tile that has been modified, used for 
-    /// recording undo actions.
-    /// </summary>
-    public struct ModifiedTileInfo
-    {
-        /// <summary>
-        /// The chunk containing the tile that was added.
-        /// </summary>
-        public HexChunk Chunk { get; private set; }
-
-        /// <summary>
-        /// Whether the chunk was just created, already existed and was modified,
-        /// or was deleted. Needed for recording what kind of Undo operation to use.
-        /// </summary>
-        public ChunkOperation Operation { get; private set; }
-
-        public enum ChunkOperation 
-        {
-            Added,
-            Modified,
-            Removed
-        }
-
-        public ModifiedTileInfo(HexChunk chunk, ChunkOperation operation) 
-        {
-            Chunk = chunk;
-            Operation = operation;
-        }
-    }
 }
